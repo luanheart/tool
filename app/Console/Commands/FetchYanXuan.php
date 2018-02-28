d4fa6cc3554efb9e384c2b9cf20560815116d65b
许锦 <xujin@corp-ci.com>
2018-02-28 10:05:58 +0800

    更换token

--- app/Console/Commands/FetchYanXuan.php
+++ app/Console/Commands/FetchYanXuan.php
@@ -22,9 +22,9 @@ class FetchYanXuan extends Command
      */
     protected $description = '网易严选自动积分签到';
 
-    public $cookie = 'YanXuanLoginCookie=;NTES_SESS=L1kpRWM3ePAoY5l4FixGd9zXb1SdzwScq5KaQ6iWzfHN2z0Hh2F0SJgtFO9reAWMaTe.xpCILgcXziJS4pItJJrkYrd.stqepK.Ezel.BCgOZ6TC.1Rus.s7Po2dcaBVsDWFUYy0KkMMS_j7PPB.p41_ANre73Q5su8Qhc4JcRJIa7xvYy7Ta3lp1XZX2kHU5;P_INFO=865022667@qq.com|1516867464|0|yanxuan|00&99|jis&1516867464&yanxuan#jis&320500#10#0#0|&0|yanxuan&wydz_platform&moneykeeper|865022667@qq.com;S_INFO=1516867464|0|##|865022667@qq.com|;yx_aui=b8d61c05-ea24-4470-9500-84bb5a158082;; yx_csrf=4c5e375029f83e45d7453ca928481d50; yx_username=865022667%40qq.com; yx_sid=b9dbaa81-da14-42b4-a7cb-07411690b997';
+    public $cookie = 'YanXuanLoginCookie=;NTES_SESS=BaPSeUS_xYwRlqde.z74rrSNiTsS4yvGhj3FWlWgwQKM1TyoG1cyPHfNc3md6rUKCu67qYSnBf.jTkHPMYnNHHGHj6n65USPFJFZGPxZIp1Xm2R72ti1fe8evBBG8FG9z7qpN3lnxCCVrmrXyslvXfKv.Ioie89uMfH9MC16f97djPdcRmKW6AQLQGkK9ADS3;P_INFO=865022667@qq.com|1519657779|0|yanxuan|00&99|jis&1519657779&yanxuan#jis&320100#10#0#0|&0|yanxuan&moneykeeper|865022667@qq.com;S_INFO=1519657779|0|##|865022667@qq.com|;yx_aui=b8d61c05-ea24-4470-9500-84bb5a158082;; yx_csrf=82a39879797fe57decbbbb76a4f00976; yx_username=865022667%40qq.com; yx_sid=7fb62619-e753-4100-85d1-40daf84a4392';
 
-    public $csrf_token = '4c5e375029f83e45d7453ca928481d50';
+    public $csrf_token = '82a39879797fe57decbbbb76a4f00976';
 
     public $api_points_sign = 'https://m.you.163.com/xhr/points/sign.json';
 
